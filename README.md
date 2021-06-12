ce941a0d5035a4880ac3ea0a3614fcf7922bffd4
unknown <tejeirodelrio@gmail.com>
2021-06-12 20:26:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1 align="center">
+  Manuel Tejeiro Del Rio - Personal blog
+</h1>
+
+This blog is based on the following gatsby-starter: https://github.com/EmaSuriano/gatsby-starter-mate
+It uses Gatsby, Netlify, Contentful and Medium articles.
---
+++ src/gatsby-theme-mate/icons.js
@@ -0,0 +1,21 @@
+import {
+    faMedium,
+    faLinkedin,
+    faTwitter,
+    faGithub,
+  } from '@fortawesome/free-brands-svg-icons';
+  import {
+    faGlobe,
+    faEnvelope,
+    faCoffee,
+  } from '@fortawesome/free-solid-svg-icons';
+  
+  export const ICONS = [
+    faMedium,
+    faTwitter,
+    faLinkedin,
+    faGithub,
+    faGlobe,
+    faEnvelope,
+    faCoffee,
+  ];
